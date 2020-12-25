6164dc2c01241d9b12502d58fa1a807a4b5787f6
sangwook <9967han@naver.com>
2020-12-26 03:15:59 +0900

    Update Lecture 10 make UserPage #1

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat
 import com.example.instagramclone.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -49,6 +50,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_account -> {
                 var userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
                 return true
             }
