6d77ba4c6cfa43e56195586b59bde51a9d6ecdd8
sangwook <9967han@naver.com>
2020-12-31 17:38:35 +0900

    Update Lecture 16 make alarm List Page

--- app/src/main/java/com/example/instagramclone/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/instagramclone/navigation/AlarmFragment.kt
@@ -5,11 +5,79 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.instagramclone.R
+import com.example.instagramclone.navigation.model.AlarmDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_alarm.view.*
+import kotlinx.android.synthetic.main.item_comment.view.*
 
 class AlarmFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
+        view.alarmfragment_recyclerview.adapter = AlarmRecyclerviewAdapter()
+        view.alarmfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
+
         return view
+    }
+    inner class AlarmRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        //알람을 저장하는 리스트 변수
+        var alarmDTOList : ArrayList<AlarmDTO> = arrayListOf()
+        init {
+            var uid = FirebaseAuth.getInstance().currentUser?.uid
+            FirebaseFirestore.getInstance().collection("alarms").whereEqualTo("destinationUid", uid).addSnapshotListener { querySnapshot, error ->
+                alarmDTOList.clear()
+                if(querySnapshot == null) return@addSnapshotListener
+
+                for(snapshot in querySnapshot.documents){
+                    alarmDTOList.add(snapshot.toObject(AlarmDTO::class.java)!!)
+                }
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view)
+
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var view = holder.itemView
+
+            //알람 -> 프로필 사진 넣어주기
+            FirebaseFirestore.getInstance().collection("profileImages").document(alarmDTOList[position].uid!!).get().addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    val url = task.result!!["image"]
+                    Glide.with(view.context).load(url).apply(RequestOptions().circleCrop()).into(view.commentviewitem_imageview_profile)
+                }
+            }
+
+            when(alarmDTOList[position].kind){
+                0 -> {
+                    var str_0 = alarmDTOList[position].userId + " " + getString(R.string.alarm_favorite)
+                    view.commentviewitem_textview_profile.text = str_0
+                }
+                1 -> {
+                    var str_1 = alarmDTOList[position].userId + " " + getString(R.string.alarm_comment) + " of " + alarmDTOList[position].message
+                    view.commentviewitem_textview_profile.text = str_1
+                }
+                2 -> {
+                    var str_2 = alarmDTOList[position].userId + " " + getString(R.string.alarm_follow)
+                    view.commentviewitem_textview_profile.text = str_2
+                }
+            }
+            view.commentviewitem_textview_comment.visibility = View.INVISIBLE
+        }
+
+        override fun getItemCount(): Int {
+            return alarmDTOList.size
+        }
     }
 }
