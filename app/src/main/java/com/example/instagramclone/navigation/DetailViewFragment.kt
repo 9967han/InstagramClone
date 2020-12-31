0012b5b21992da408534f5e5c40100e4d7678dc3
sangwook <9967han@naver.com>
2020-12-31 15:34:18 +0900

    Update Lecture 14 make Comment Page

--- app/src/main/java/com/example/instagramclone/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/example/instagramclone/navigation/DetailViewFragment.kt
@@ -1,5 +1,6 @@
 package com.example.instagramclone.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Layout
 import android.view.LayoutInflater
@@ -99,6 +100,11 @@ class DetailViewFragment : Fragment() {
                 bundle.putString("userId", contentDTOs[p1].userId)
                 fragment.arguments = bundle
                 activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.main_content, fragment)?.commit()
+            }
+            viewholder.detailviewitem_comment_imageview.setOnClickListener { v ->
+                var intent = Intent(v.context, CommentActivity::class.java)
+                intent.putExtra("contentUid", contentUidList[p1])
+                startActivity(intent)
             }
         }
 
